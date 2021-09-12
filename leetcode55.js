5e58e6b0b93ade59ba6f463443c9ae2762caedcc
Sean Yang <eiffel205566@hotmail.com>
2021-09-12 16:42:54 -0400

    add some comments for leetcode 55

--- leetcode55.js
+++ leetcode55.js
@@ -5,11 +5,12 @@
  * https://leetcode.com/submissions/detail/553834947/
  */
 var canJump = function (nums) {
+  //edge case when length is only 1
   if (nums.length === 1) return true;
 
   let maxReach = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (maxReach >= nums.length - 1) return true;
+    if (maxReach >= nums.length - 1) return true; //when we already reached the end of array
 
     if (nums[i] === 0) {
       if (maxReach <= i) {
