5cacd8a8b298bbda1b84277ecb2e85ebe9b8ce4f
Sean Yang <eiffel205566@hotmail.com>
2021-10-12 11:11:35 -0400

    leetcode264 a better version, 50% faster, still slow though

--- leetcode264.js
+++ leetcode264.js
@@ -31,3 +31,30 @@ var nthUglyNumber = function (n) {
 
   return arr.pop();
 };
+
+var nthUglyNumber2 = function (n) {
+  let nums = {
+    2: 0,
+    3: 0,
+    5: 0,
+  };
+
+  let arr = [1];
+
+  if (n < 2) return 1;
+
+  while (arr.length != n) {
+    let keys = [];
+    // get the min by trying out all choices in nums
+    let min = Math.min(...Object.keys(nums).map(key => key * arr[nums[key]]));
+    // if that choice of key result a min value, then add to keys arr
+    for (let key in nums) {
+      if (key * arr[nums[key]] === min) keys.push(key);
+    }
+    // increment value by 1 where value represents index in arr;
+    keys.forEach(k => (nums[k] += 1));
+    arr.push(min);
+  }
+
+  return arr.pop();
+};
