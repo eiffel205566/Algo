1bf9836c749f373a4732c38d6168dd53de20068d
Sean Yang <eiffel205566@hotmail.com>
2022-06-11 17:01:49 -0400

    update leetcode3

--- leetcode3.js
+++ leetcode3.js
@@ -1,11 +1,17 @@
 /**
  * @param {string} s
  * @return {number}
+ * https://leetcode.com/submissions/detail/719945213/
+ * beat 86%
  */
 var lengthOfLongestSubstring = function (s) {
   let pointerOne = 0;
   let pointerTwo = 0;
   let final = 0;
+
+  if (s.length <= 1) {
+    return s.length;
+  }
 
   for (let i = 0; i < s.length; i++) {
     pointerTwo = i; // update pointerTwo
